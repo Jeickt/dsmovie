f310c1e9edc6861d9d85a97939b6d45596e16883
Jeison Leandro Rückert <jeison.let@gmail.com>
2022-01-12 15:51:08 -0300

    Acertando import do svg github

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ReactComponent as GithubIcon } from 'assets/img/github.svg';
+import { ReactComponent as GithubIcon } from '../../assets/img/github.svg';
 import './styles.css';
 
 function Navbar() {
