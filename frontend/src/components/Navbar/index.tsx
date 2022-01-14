8c13ddee54325f1390313273ad6546a42ace332a
Jeison Leandro Rückert <jeison.let@gmail.com>
2022-01-14 15:47:41 -0300

    utf-8

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { ReactComponent as GithubIcon } from 'assets/img/github.svg';
 import './styles.css';
 
 function Navbar() {
@@ -10,7 +9,6 @@ function Navbar() {
           <h1>DSMovie</h1>
           <a href="https://github.com/Jeickt" target="_blank" rel="noreferrer">
             <div className="dsmovie-contact-container">
-              <GithubIcon />
               <p className="dsmovie-contact-link">/Jeickt</p>
             </div>
           </a>
